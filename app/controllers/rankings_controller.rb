8114b2c4307e9f33a6d0a44fef6b3c4607ccf6b4
Shuichi <buzzplusmoney@gmail.com>
2019-02-23 11:54:54 +0000

    layout-fixed

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,9 +1,15 @@
 class RankingsController < ApplicationController
   def index
 	@ranking = UsersHobby.group(:hobby_id).order('count(hobby_id) desc')
-	men = User.find_by(sex: '男性')
-	women = User.find_by(sex: '女性')
-	@men = UsersHobby.where(user_id: men.id).group(:hobby_id).order('count(hobby_id) desc')
+	men = User.where(sex: '男性').pluck(:id)
+	women = User.where(sex: '女性').pluck(:id)
+	young = User.where(age: ['10','20','30']).pluck(:id)
+	middle = User.where(age: ['40','50','60']).pluck(:id)
+	older = User.where(age: ['70','80','90']).pluck(:id)
+	@men = UsersHobby.where(user_id: men).group(:hobby_id).order('count(hobby_id) desc')
 	@women = UsersHobby.where(user_id: women).group(:hobby_id).order('count(hobby_id) desc')
+	@young = UsersHobby.where(user_id: young).group(:hobby_id).order('count(hobby_id) desc')
+	@middle = UsersHobby.where(user_id: middle).group(:hobby_id).order('count(hobby_id) desc')
+	@older = UsersHobby.where(user_id: older).group(:hobby_id).order('count(hobby_id) desc')
   end
 end
